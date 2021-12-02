2c3ca3c3c779ce4fa2b617508e33999e8cbcad5b
변찬우 <chan00106@naver.com>
2021-12-02 17:51:20 +0900

    page setting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import * as P from "./Pages";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<P.Main />} />
+      <Route path="/login" element={<P.Login />} />
+    </Routes>
+  );
 }
 
 export default App;
