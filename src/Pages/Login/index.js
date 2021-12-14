5e995a7968cd889c4892f4a875f911c75f4ccdc0
변찬우 <chan00106@naver.com>
2021-12-14 19:34:57 +0900

    add login page

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { LogoSVG } from "../../Asset/SVG";
+import { LoginWrapper } from "./style";
+import GoogleIcon from "@mui/icons-material/Google";
 
 export default function Login() {
   return (
-    <div>
-      <h1>login</h1>
-    </div>
+    <LoginWrapper>
+      <div className="left">
+        <LogoSVG />
+        <h1>
+          다른 사람들의 포트폴리오가
+          <br />
+          궁금하진 않으신가요?
+        </h1>
+        <p>포트폴리오를 제작하고 다른사람과 공유해 보세요</p>
+        <button>
+          <div className="svg">
+            <GoogleIcon />
+          </div>
+          Sign up with google
+        </button>
+      </div>
+      <div className="right"></div>
+    </LoginWrapper>
   );
 }
