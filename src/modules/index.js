280e28062a745a464fe2fbef0d0ac14f4b8679ee
변찬우 <chan00106@naver.com>
2021-12-17 11:48:44 +0900

    add userRedux and update App.js

--- src/modules/index.js
+++ src/modules/index.js
@@ -2,7 +2,8 @@ import { combineReducers } from "redux";
 import notice from "./notice";
 import posts from "./posts";
 import myInfo from "./myInfo";
+import user from "./user";
 
-const reducer = combineReducers({ posts, notice, myInfo });
+const reducer = combineReducers({ posts, notice, myInfo, user });
 
 export default reducer;
