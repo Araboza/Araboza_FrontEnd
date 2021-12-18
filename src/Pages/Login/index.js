47d161afe4ef1ea14b24eb7ca29d252732ed76dc
변찬우 <chan00106@naver.com>
2021-12-18 14:42:15 +0900

    Update index.js

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { LogoSVG } from "../../Asset/SVG";
-import { LoginWrapper } from "./style";
+import * as I from "../../Asset/SVG";
+import * as S from "./style";
 import GoogleLogin from "react-google-login";
 // import { PostToken } from "../../lib/api/postToken";
 
@@ -10,10 +10,10 @@ export default function Login() {
     // PostToken(res.tokenId);
   };
   return (
-    <LoginWrapper>
+    <S.LoginWrapper>
       <div className="left">
         <div>
-          <LogoSVG />
+          <I.LogoSVG />
           <h1>
             다른 사람들의 포트폴리오가
             <br />
@@ -29,6 +29,6 @@ export default function Login() {
         </div>
       </div>
       <div className="right"></div>
-    </LoginWrapper>
+    </S.LoginWrapper>
   );
 }
