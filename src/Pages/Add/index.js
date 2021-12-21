ca9027db0a18665cea61d31319e690c128dd7386
변찬우 <chan00106@naver.com>
2021-12-21 23:32:25 +0900

    add markdown editor 만들기

--- src/Pages/Add/index.js
+++ src/Pages/Add/index.js
@@ -1,9 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import Header from "../../components/Header";
+import ReactQuill, { Quill } from "react-quill";
+import MarkdownShortcuts from "quill-markdown-shortcuts";
+
+import "react-quill/dist/quill.bubble.css";
+import * as S from "./style";
+
+Quill.register("modules/markdownShortcuts", MarkdownShortcuts);
+
+const modules = {
+  toolbar: [
+    //[{ 'font': [] }],
+    [{ header: [1, 2, false] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [{ list: "ordered" }, { list: "bullet" }],
+    ["link", "image"],
+    [{ align: [] }, { color: [] }, { background: [] }], // dropdown with defaults from theme
+    ["clean"],
+  ],
+  markdownShortcuts: {},
+};
+
+const formats = [
+  //'font',
+  "header",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+  "link",
+  "image",
+  "align",
+  "color",
+  "background",
+];
 
 export default function Add() {
+  const [value, setValue] = useState();
+
+  const onChange = (mark) => {
+    console.log(mark);
+    setValue(mark);
+  };
+
   return (
-    <div>
-      <h1>Add</h1>
-    </div>
+    <>
+      <Header />
+      <S.AddWrapper>
+        <input type="text" placeholder="제목을 입력하세요" />
+        <ReactQuill
+          style={{ height: "65vh" }}
+          placeholder="글을 작성하세요"
+          theme="bubble"
+          modules={modules}
+          formats={formats}
+          value={value || ""}
+          onChange={(content, delta, source, editor) =>
+            onChange(editor.getHTML())
+          }
+        />
+      </S.AddWrapper>
+    </>
   );
 }
---
+++ src/Pages/Add/style.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const AddWrapper = styled.div`
+  width: 70vw;
+  margin: 0 auto;
+
+  input {
+    width: 100%;
+    border: none;
+    border-bottom: 1px solid #e2e2e2;
+    outline: none;
+    height: 7vh;
+    background: none;
+    font-size: 2.3rem;
+    font-weight: bold;
+    margin-bottom: 20px;
+  }
+
+  .ql-container {
+    font-size: 1.3rem;
+  }
+`;
